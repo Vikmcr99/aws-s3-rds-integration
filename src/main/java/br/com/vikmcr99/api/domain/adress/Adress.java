9936fd5b287c03be68f60992eb2c9a5bea9aae1b
victor <victor.robinson@al.infnet.edu.br>
2025-03-27 19:35:16 -0300

    feat: creating entities

---
+++ src/main/java/br/com/vikmcr99/api/domain/adress/Adress.java
@@ -0,0 +1,26 @@
+package br.com.vikmcr99.api.domain.adress;
+
+import br.com.vikmcr99.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Table(name = "adress")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Adress {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
---
+++ src/main/java/br/com/vikmcr99/api/domain/event/Event.java
@@ -0,0 +1,31 @@
+package br.com.vikmcr99.api.domain.event;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "event")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Event {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String title;
+    private String description;
+    private String imageUrl;
+    private String eventUrl;
+    private Boolean remote;
+    private Date date;
+
+}
