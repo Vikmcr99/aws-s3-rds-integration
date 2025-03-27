2bcf0f276fe2f73b862ca17209873d434f1301eb
victor <victor.robinson@al.infnet.edu.br>
2025-03-27 19:34:58 -0300

    feat: first commit

---
+++ src/test/java/br/com/vikmcr99/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vikmcr99.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
