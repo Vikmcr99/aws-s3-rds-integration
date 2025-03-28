f59cc1a3599b2a42cad546fe34f17c4ce593e4d0
victor <victor.robinson@al.infnet.edu.br>
2025-03-28 19:35:21 -0300

    feat: creating migrations

---
+++ src/main/resources/db/migration/V3__create-adress-table.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE adress(
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    city VARCHAR (100) NOT NULL,
+    uf VARCHAR (100) NOT NULL,
+    event_id UUID,
+    FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE
+
+
+);
