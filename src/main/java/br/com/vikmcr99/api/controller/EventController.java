e55f0d9dcc32d4e44ee3e89b9bc99c500293fe96
victor <victor.robinson@al.infnet.edu.br>
2025-04-02 17:01:50 -0300

    feat: creating pagination

--- src/main/java/br/com/vikmcr99/api/controller/EventController.java
+++ src/main/java/br/com/vikmcr99/api/controller/EventController.java
@@ -2,12 +2,15 @@ package br.com.vikmcr99.api.controller;
 
 import br.com.vikmcr99.api.domain.event.Event;
 import br.com.vikmcr99.api.domain.event.EventRequestDTO;
+import br.com.vikmcr99.api.domain.event.EventResponseDTO;
 import br.com.vikmcr99.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/event")
@@ -27,6 +30,12 @@ public class EventController {
                                              @RequestParam(value = "image", required = false)MultipartFile image) {
         EventRequestDTO eventRequestDTO = new EventRequestDTO(title, description, date, city, state, remote, eventUrl, image);
         return new ResponseEntity<>(eventService.createEvent(eventRequestDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDTO>> getAllEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10" ) int size) {
+        List<EventResponseDTO> events = eventService.getAllEvents(page, size);
+        return new ResponseEntity<List<EventResponseDTO>>(events, HttpStatus.OK);
     }
 
 }
--- src/main/java/br/com/vikmcr99/api/service/EventService.java
+++ src/main/java/br/com/vikmcr99/api/service/EventService.java
@@ -2,10 +2,14 @@ package br.com.vikmcr99.api.service;
 
 import br.com.vikmcr99.api.domain.event.Event;
 import br.com.vikmcr99.api.domain.event.EventRequestDTO;
+import br.com.vikmcr99.api.domain.event.EventResponseDTO;
 import br.com.vikmcr99.api.repositories.EventRepository;
 import com.amazonaws.services.s3.AmazonS3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,6 +18,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -72,5 +77,12 @@ public class EventService {
         fos.close();
 
         return convertFile;
+    }
+
+    public List<EventResponseDTO> getAllEvents(int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = eventRepository.findAll(pageable);
+        return eventsPage.map(event -> new EventResponseDTO(event.getId(), event.getTitle(), event.getDescription(), event.getDate(), "", "",
+                event.getRemote(), event.getEventUrl(), event.getImageUrl())).stream().toList();
     }
 }
